3d8e7f27ca8591b3e407f1e41004fa7afcd0c825
clearloop <clearloop@users.noreply.github.com>
2022-03-04 17:07:16 +0800

    chore(license): add ChainSafe headers to rust files

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+// Copyright 2021 ChainSafe Systems
+// SPDX-License-Identifier: LGPL-3.0-only
+
 //! the garii project
 //!
 //! this library fetches blocks from Arweave and generates firehose blocks for the Graph
