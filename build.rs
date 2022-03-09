42b994423d2e1b0f6e51ae00950f30966782323e
Jovansonlee Cesar <ivanceras@gmail.com>
2022-03-10 03:03:18 +0800

    feat: add proto files and build it using tonic-build (#22)
    * feat: add proto files and build it using tonic-build
    
    * fix: error when using complex types Timestamp
    
    * fix: use string from u64 on fields: diff, weave_size and block_size
    
    * add the license to the generated files from .proto

---
+++ build.rs
@@ -0,0 +1,11 @@
+// Copyright 2021 ChainSafe Systems
+// SPDX-License-Identifier: LGPL-3.0-only
+
+use std::io::Result;
+fn main() -> Result<()> {
+    println!("cargo:rerun-if-changed=proto");
+    tonic_build::configure()
+        .out_dir("src/protobuf")
+        .format(true)
+        .compile(&["proto/block.proto"], &["proto"])
+}
