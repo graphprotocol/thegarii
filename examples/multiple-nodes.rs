d529b865af345f8d84f19ef1e42b25dcbb52e36d
willesxm <willeslau@gmail.com>
2022-03-11 23:53:16 +0800

    pulling multiple nodes

---
+++ examples/multiple-nodes.rs
@@ -0,0 +1,21 @@
+use thegarii::Extractor;
+use std::time::Instant;
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+
+    let start = Instant::now();
+    let nodes = vec![
+        "https://arweave.net/"
+    ];
+    let mut extractor = Extractor::new(nodes);
+
+    match extractor.pull(269512u64, 269513u64).await {
+        Ok(_) => {
+            let elapsed = start.elapsed();
+            log::info!("done, used: {:?}", elapsed);
+        },
+        Err(e) => log::error!("failed with reason: {:?}", e)
+    }
+}
--- src/extractor.rs
+++ src/extractor.rs
@@ -4,11 +4,14 @@
 //! Arweave block pulling orchestrator. It utilizes the Client to pull
 //! data from different blocks based on the input configuration.
 
-use crate::result::Result;
-use crate::types::Transaction;
-use crate::Client;
 use rand::Rng;
 use std::sync::Arc;
+use std::thread;
+use std::time::Duration;
+use tokio::sync::Mutex;
+use log;
+use crate::result::Result;
+use crate::Client;
 
 /// The Extractor struct that handles the pulling of different nodes
 pub struct Extractor {
@@ -16,22 +19,20 @@ pub struct Extractor {
     clients: Vec<Arc<Client>>,
     /// The next node to query data from
     next_node: usize,
-    /// The sender to the storage thread
-    storage_sender: Arc<tokio::sync::mpsc::Sender<Transaction>>,
 }
 
 impl Extractor {
-    pub fn new(nodes: Vec<&'static str>, sender: tokio::sync::mpsc::Sender<Transaction>) -> Self {
+    pub fn new(nodes: Vec<&'static str>) -> Self {
         Self {
             clients: nodes.iter().map(|n| Arc::new(Client::new(n))).collect(),
             next_node: 0,
-            storage_sender: Arc::new(sender),
         }
     }
 
     /// Start pulling from the clients from the start to the
     /// end blocks, exclusive of the end block
     pub async fn pull(&mut self, start: u64, end: u64) -> Result<()> {
+        log::info!("pulling block from {:?} to {:?}", start, end-1);
         for block in start..end {
             self.pull_block(block).await?;
         }
@@ -42,19 +43,44 @@ impl Extractor {
         let client = self.select_random_client();
         let block = client.get_block_by_height(height).await?;
 
+        log::debug!("pulled block at height: {:?}, # txns is {:}", height, block.txs.len());
+
+        // This is just a simple count down latch implementation using mutex.
+        // There should be better solutions in actual prod implementation.
+        // For testing, this should be enough.
+        let simple_countdown = Arc::new(Mutex::new(block.txs.len()));
         for t in block.txs {
             let c = Arc::clone(&self.clients[self.next_node]);
-            let s = Arc::clone(&self.storage_sender);
+            let l = Arc::clone(&simple_countdown);
+
+            // increment the next node idx
             self.next_node = (self.next_node + 1) % self.clients.len();
+
             tokio::spawn(async move {
                 match c.get_tx_by_id(&t).await {
-                    Ok(tx) => s.send(tx).await,
-                    Err(_) => {
-                        todo!()
+                    Ok(tx) => {
+                        // we should store to rocks db
+                        log::debug!("fetched transaction: {:?}", tx.id);
+                    },
+                    Err(e) => {
+                        log::error!("todo, handle this error: {:?}", e);
                     }
                 }
+                let ref l_ref = &*l;
+                let mut r = l_ref.lock().await;
+                *r -= 1;
             });
         }
+
+        loop {
+            let countdown = simple_countdown.lock().await;
+            log::debug!("countdown: {:?}", countdown);
+            if countdown.eq(&0) { break; }
+            thread::sleep(Duration::from_millis(1000));
+        }
+
+        log::info!("finished pulling block {:?}", height);
+
         Ok(())
     }
 
