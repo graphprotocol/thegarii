58e20f9db2640c05140bd7771a2d4a0b31b0f047
clearloop <26088946+clearloop@users.noreply.github.com>
2022-03-25 00:51:30 +0800

    feat(grpc): include protobuf files from OUR_DIR

--- src/pb.rs
+++ src/pb.rs
@@ -1,2 +1,5 @@
 // Copyright 2021 ChainSafe Systems
 // SPDX-License-Identifier: LGPL-3.0-only
+
+tonic::include_proto!("block");
+tonic::include_proto!("sf.firehose.v1");
---
+++ src/service/grpc.rs
@@ -0,0 +1,5 @@
+// Copyright 2021 ChainSafe Systems
+// SPDX-License-Identifier: LGPL-3.0-only
+
+//! gRPC service
+pub struct RPC {}
