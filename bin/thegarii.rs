582add8f351ffb6bb30a5b9a799fdcb0e3a95291
Matthieu Vachon <matt@streamingfast.io>
2022-05-06 13:39:28 -0400

    Added ctrl-c signal handling and better error printing on exit

--- bin/thegarii.rs
+++ bin/thegarii.rs
@@ -1,9 +1,31 @@
 // Copyright 2021 ChainSafe Systems
 // SPDX-License-Identifier: LGPL-3.0-only
 
+use std::process::exit;
+
 use thegarii::Opt;
+use tokio::signal;
 
 #[tokio::main]
-async fn main() {
-    Opt::exec().await.expect("thegraii crashed")
+async fn main() -> Result<(), anyhow::Error> {
+    tokio::spawn(async {
+        if let Err(err) = signal::ctrl_c().await {
+            log::error!("error waiting for SIGINT signal: {:?}", err);
+            exit(1);
+        }
+
+        log::info!("received SIGINT signal, terminating");
+        exit(0);
+    });
+
+    match Opt::exec().await {
+        Ok(_) => {
+            log::info!("completed");
+            Ok(())
+        }
+        Err(err) => {
+            log::error!("unexpected error occurred: {:?}", err);
+            exit(1);
+        }
+    }
 }
